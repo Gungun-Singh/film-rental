294d07da4c5b63e69fdff9850395d636eb1801f7
Rhythm Gupta <aceone1904@gmail.com>
2026-03-28 14:27:21 +0530

    Controller are made empty files
    controllers are made

---
+++ src/main/java/com/filmrental/controller/StoreController.java
@@ -0,0 +1,9 @@
+package com.filmrental.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stores")
+public class StoreController {
+}
