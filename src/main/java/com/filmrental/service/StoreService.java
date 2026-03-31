8b2b317cd2fe4d7b15ba579941d68d64543f3e74
Aakanshi-garg <aakanshigarg62@gmail.com>
2026-03-31 18:46:40 +0530

    Added changes to StoreService

--- src/main/java/com/filmrental/service/StoreService.java
+++ src/main/java/com/filmrental/service/StoreService.java
@@ -1,8 +1,12 @@
 package com.filmrental.service;
 
+import com.filmrental.dto.response.StoreInventoryResponse;
 import com.filmrental.dto.response.StoreResponse;
+
+import java.util.List;
 
 public interface StoreService {
     StoreResponse getStoreById(Integer storeId);
     StoreResponse getManagerById(Integer managerStaffId);
+    List<StoreInventoryResponse> getStoreInventory(Integer storeId);
 }
