0685bc050e9a8a159deeea4528de29ee2998ed8c
Khushi-Ranjan <khushiranjan2004@gmail.com>
2026-03-31 15:49:40 +0530

    Added method to fetch films by ActorId in FilmRepository

--- src/main/java/com/filmrental/repository/FilmRepository.java
+++ src/main/java/com/filmrental/repository/FilmRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface FilmRepository extends JpaRepository<Film, Integer> {
     List<Film> findByCategories_CategoryId(Integer categoryId);
+    List<Film> findByActors_ActorId(Integer actorId);
 }
