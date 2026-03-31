7620666cdc377fe45a0b2817c1112fc7dd969404
Gungun Singh <gunguns618@gmail.com>
2026-03-31 14:48:03 +0530

    modified Customer controller by adding the endpoint

--- src/main/java/com/filmrental/controller/CustomerController.java
+++ src/main/java/com/filmrental/controller/CustomerController.java
@@ -1,10 +1,12 @@
 package com.filmrental.controller;
 
 import com.filmrental.dto.response.CustomerResponse;
+import com.filmrental.dto.response.RentalResponse;
 import com.filmrental.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/customers")
@@ -13,8 +15,16 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
+    //1st API - GET/customers/{customer_id}
     @GetMapping("/{customerId}")
     public ResponseEntity<CustomerResponse> getCustomerById(@PathVariable Integer customerId) {
         return ResponseEntity.ok(customerService.getCustomerById(customerId));
+    }
+
+
+    // 2nd API - GET /customers/{customer_id}/rentals
+    @GetMapping("/{customerId}/rentals")
+    public ResponseEntity<List<RentalResponse>> getCustomerRentals(@PathVariable Integer customerId) {
+        return ResponseEntity.ok(customerService.getCustomerRentals(customerId));
     }
 }
