0f55905b7b0422b651ff29d4d3ae6aec96c54c6a
Khushi-Ranjan <khushiranjan2004@gmail.com>
2026-03-31 13:47:06 +0530

    modified FilmService Logic for GET FilmByCategoryId api

--- src/main/java/com/filmrental/service/FilmService.java
+++ src/main/java/com/filmrental/service/FilmService.java
@@ -4,4 +4,5 @@ import com.filmrental.dto.response.FilmResponse;
 import java.util.List;
 public interface FilmService {
     FilmResponse getFilmById(Integer id);
+    List<FilmResponse> getFilmsByCategoryId(Integer categoryId);
 }
