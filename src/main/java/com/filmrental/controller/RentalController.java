c6e8c5c02142d65f5510b06f4c50026de00521ab
RhythmGupta1920 <162402480+RhythmGupta1920@users.noreply.github.com>
2026-04-01 10:13:15 +0530

    Update API version in RentalController mapping

--- src/main/java/com/filmrental/controller/RentalController.java
+++ src/main/java/com/filmrental/controller/RentalController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@RestController @RequestMapping("/api/rentals") @RequiredArgsConstructor
+@RestController @RequestMapping("/api/v1/rentals") @RequiredArgsConstructor
 public class RentalController {
     private final RentalService rentalService;
 
