48901e62ae2a910cbb9839611bbc6125f6c34c6f
Khushi-Ranjan <khushiranjan2004@gmail.com>
2026-04-03 16:43:02 +0530

    refactor: prune unused API code paths and align tests

--- src/main/java/com/filmrental/service/FilmService.java
+++ src/main/java/com/filmrental/service/FilmService.java
@@ -1,7 +1,9 @@
 package com.filmrental.service;
-import com.filmrental.dto.request.CreateFilmRequest;
+
 import com.filmrental.dto.response.FilmResponse;
+
 import java.util.List;
+
 public interface FilmService {
     FilmResponse getFilmById(Integer id);
     List<FilmResponse> getFilmsByCategoryId(Integer categoryId);
